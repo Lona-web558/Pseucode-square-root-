33990637d8131400839dc559aaf0fdca64c9f039
Lona Matshingana <71089848+Lona-web558@users.noreply.github.com>
2025-02-18 09:29:07 +0200

    Add files via upload

---
+++ Square root.py
@@ -0,0 +1,24 @@
+#Pseudocode for finding square root
+#of a number
+
+#begin
+
+#ENTER y
+
+#Process
+
+#y^2 = x
+
+#RESULT
+#OUTPUT is the square root
+
+#ALGORITHM
+
+integer_value1 = input("Enter a integer : ")
+num1 = int(integer_value1)
+print("The integer is : ", num1)
+
+#find square root
+
+square = num1**2
+print("The square root is", square)
